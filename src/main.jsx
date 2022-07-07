56719a85e74f5cb17b7ff3488065f88139883dde
GootaLK <lokosmen1235@gmail.com>
2022-07-07 17:44:51 -0500

    Feat: Add global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './app'
 
+import './styles/global.css'
+
 const container = document.getElementById('root')
 const root = createRoot(container)
 
