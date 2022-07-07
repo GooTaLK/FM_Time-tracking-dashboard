01e1760f07fd5eefe15d540cd7e4b9cf07633610
GootaLK <lokosmen1235@gmail.com>
2022-07-07 17:45:48 -0500

    Feat: Add color variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './app'
 
 import './styles/global.css'
+import './styles/colors.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
