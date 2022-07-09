a54e27a11dc7caa6f35ae0c193721340cea3da21
GootaLK <lokosmen1235@gmail.com>
2022-07-08 22:52:51 -0500

    Feat: Add autoprefixer package

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
 })
