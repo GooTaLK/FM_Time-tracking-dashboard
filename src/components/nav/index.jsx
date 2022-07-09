7b350266ac5278ed5b97132980ee59fc4dae2f9b
GootaLK <lokosmen1235@gmail.com>
2022-07-09 01:31:03 -0500

    Feat: Add currentLink property to Nav

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -3,24 +3,37 @@ import utilsStyles from '../../styles/utils.module.css'
 import jeremyImage from '../../../assets/images/image-jeremy.png'
 import ProfileSpace from './profileSpace'
 
-export default function Nav() {
+export default function Nav({ currenLink }) {
   return (
     <div className={`NavWrapper ${utilsStyles.rounded}`}>
       <ProfileSpace imgSrc={jeremyImage} name='Jeremy Robson' />
       <nav className='Nav'>
         <ul>
           <li>
-            <a className='Nav-links' href=''>
+            <a
+              className={`Nav-links${currenLink === 'daily' ? ' current' : ''}`}
+              href='#daily'
+            >
               Daily
             </a>
           </li>
           <li>
-            <a className='Nav-links' href=''>
+            <a
+              className={`Nav-links${
+                currenLink === 'weekly' ? ' current' : ''
+              }`}
+              href='#weekly'
+            >
               Weekly
             </a>
           </li>
           <li>
-            <a className='Nav-links' href=''>
+            <a
+              className={`Nav-links${
+                currenLink === 'monthly' ? ' current' : ''
+              }`}
+              href='#monthly'
+            >
               Monthly
             </a>
           </li>
