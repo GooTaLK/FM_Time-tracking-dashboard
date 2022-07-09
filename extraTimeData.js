9cebf87b5c89923db17aada72a7af11e42942f5e
GootaLK <lokosmen1235@gmail.com>
2022-07-08 22:59:14 -0500

    Feat: Create a file with more data

---
+++ extraTimeData.js
@@ -0,0 +1,44 @@
+export const extraTimeData = {
+  work: {
+    topImage: {
+      src: '../../../assets/svg/icon-work.svg',
+      alt: 'Briefcase icon',
+    },
+    backgroundColor: 'var(--light-orange)',
+  },
+  play: {
+    topImage: {
+      src: '../../../assets/svg/icon-play.svg',
+      alt: 'Remote Console icon',
+    },
+    backgroundColor: 'var(--soft-blue)',
+  },
+  study: {
+    topImage: {
+      src: '../../../assets/svg/icon-study.svg',
+      alt: 'Book icon',
+    },
+    backgroundColor: 'var(--light-red)',
+  },
+  exercise: {
+    topImage: {
+      src: '../../../assets/svg/icon-exercise.svg',
+      alt: 'Person running icon',
+    },
+    backgroundColor: 'var(--lime-green)',
+  },
+  social: {
+    topImage: {
+      src: '../../../assets/svg/icon-social.svg',
+      alt: 'Dialog box icon',
+    },
+    backgroundColor: 'var(--violet)',
+  },
+  'self care': {
+    topImage: {
+      src: '../../../assets/svg/icon-self-care.svg',
+      alt: 'Heart icon',
+    },
+    backgroundColor: 'var(--soft-orange)',
+  },
+}
