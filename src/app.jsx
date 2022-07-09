71879f4df925c0e3b7eb8c146f2285ae19b2e9da
GootaLK <lokosmen1235@gmail.com>
2022-07-09 01:35:25 -0500

    Feat: Add behavior to change date type depending on hash

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,43 @@
+import { useEffect, useRef, useState } from 'react'
 import './app.css'
 import Nav from './components/nav'
 import TimeContainer from './components/timeContainer'
 import timeData from '../data.json'
 
+function getTypeFromHash() {
+  if (location.hash === '#monthly') return 'monthly'
+  if (location.hash === '#weekly') return 'weekly'
+  return 'daily'
+}
+
 export default function App() {
+  const [dateType, setDateType] = useState(getTypeFromHash())
+  const dateTypeRef = useRef(dateType)
+
+  dateTypeRef.current = dateType
+
+  useEffect(() => {
+    function hashchangeHandler() {
+      const newDateType = getTypeFromHash()
+
+      if (location.hash.slice(1) !== newDateType) {
+        location.hash = newDateType
+        setDateType(newDateType)
+      } else if (dateTypeRef !== newDateType) {
+        setDateType(newDateType)
+      }
+    }
+
+    location.hash = getTypeFromHash()
+    window.addEventListener('hashchange', hashchangeHandler)
+
+    return () => window.removeEventListener('hashchange', hashchangeHandler)
+  }, [])
+
   return (
     <div className='App'>
-      <Nav />
-      <TimeContainer timeData={timeData} type='daily' />
+      <Nav currenLink={dateType} />
+      <TimeContainer timeData={timeData} type={dateType} />
     </div>
   )
 }
